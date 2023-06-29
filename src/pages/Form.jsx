21160db0e3ef0b2e7426d58d0ba5497e847f86d0
Leônidas Augusto Amaro <augustoamaro@proton.me>
2023-06-29 17:05:21 -0300

    .

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -67,7 +67,6 @@ const Form = () => {
     }
   };
 
-
   return (
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
       {celebrate && <Confetti width={width} height={height} />}
