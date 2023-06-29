161c7f22a75df813b7a9800a0032175b7ae4f891
Marlon Frade <marlonmelo98@hotmail.com>
2023-06-29 09:38:45 -0300

    feat(*): initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button variant="contained">Hello World</Button>
+    </>
+  );
+}
