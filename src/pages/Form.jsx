fb3da3c5464cb0ef9429d7f5fb214c02822985b5
Leônidas Augusto Amaro <augustoamaro@proton.me>
2023-06-29 16:21:18 -0300

    Working with loading in Form page and confetti

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../firebase";
 import { v4 as uuidv4 } from "uuid";
+import { CircularProgress } from "@material-ui/core";
+import { useWindowSize } from 'react-use';
+import Confetti from 'react-confetti';
 
 import { Switch } from "@headlessui/react";
 import MenuList from "../components/MenuList";
@@ -13,16 +16,20 @@ function classNames(...classes) {
 const Form = () => {
   const [agreed, setAgreed] = useState(false);
   const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [celebrate, setCelebrate] = useState(false);
+  
   let date = new Date();
   const postCollectionRef = collection(db, "tweets");
+  const { width, height } = useWindowSize();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setLoading(true);
 
     const url = "http://204.48.19.221:3000/api/text/GPT3_5ThreeTries";
     const payload = {
-      prompt:
-        "Seu nome agora é Lucy. Você é uma mente mestra em comentar frases de uma forma que as pessoas se engajem e se divirtam. Você como mente mestra deve ler as frases que serão enviadas e fazer um comentário bem estruturado e que seja divertido de até 200 caracteres. Utilize piadas, curiosidades e responda sempre para o usuário que enviou o comentário. Procure não repetir nada da frase que o usuário enviou. Você deve utilizar elementos ligados a festa junina, turismo e tecnologia. Lucy, responda agora o comentário",
+      prompt: "Seu nome agora é Lucy. Você é uma mente mestra em comentar frases de uma forma que as pessoas se engajem e se divirtam. Você como mente mestra deve ler as frases que serão enviadas e fazer um comentário bem estruturado e que seja divertido de até 200 caracteres. Utilize piadas, curiosidades e responda sempre para o usuário que enviou o comentário. Procure não repetir nada da frase que o usuário enviou. Você deve utilizar elementos ligados a festa junina, turismo e tecnologia. Lucy, responda agora o comentário",
       temperature: 1,
       message: message,
     };
@@ -41,37 +48,37 @@ const Form = () => {
       }
 
       const data = await response.json();
-      console.log(data);
+      
+      if (data.status === "success") {
+        const fullMessageData = {
+          id: uuidv4(),
+          date,
+          message: payload.message,
+          comment: data.data,
+        };
 
-      const fullMessageData = {
-        id: uuidv4(),
-        date,
-        message: payload.message,
-        comment: data.data,
-      };
-      console.log(fullMessageData);
+        await addDoc(postCollectionRef, fullMessageData);
+        setCelebrate(true);
 
-      await addDoc(postCollectionRef, fullMessageData).then((result) => {
-        console.log(result);
-      });
+        setTimeout(() => {
+          setCelebrate(false);
+        }, 5000);
+      }
+
     } catch (error) {
       console.error("Error:", error);
     }
+
+    setLoading(false);
   };
 
   return (
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+      {celebrate && <Confetti width={width} height={height} />}
       <div
         className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
         aria-hidden="true"
       >
-        <div
-          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
       </div>
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
@@ -91,7 +98,6 @@ const Form = () => {
           <div className="sm:col-span-2">
             <MenuList />
           </div>
-
           <div className="sm:col-span-2">
             <label
               htmlFor="message"
@@ -101,7 +107,6 @@ const Form = () => {
             </label>
             <div className="mt-2.5">
               <textarea
-                name="message"
                 id="message"
                 rows={4}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -110,42 +115,15 @@ const Form = () => {
               />
             </div>
           </div>
-          <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
-            <div className="flex h-6 items-center">
-              <Switch
-                checked={agreed}
-                onChange={setAgreed}
-                className={classNames(
-                  agreed ? "bg-indigo-600" : "bg-gray-200",
-                  "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                )}
-              >
-                <span className="sr-only">Agree to policies</span>
-                <span
-                  aria-hidden="true"
-                  className={classNames(
-                    agreed ? "translate-x-3.5" : "translate-x-0",
-                    "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
-                  )}
-                />
-              </Switch>
-            </div>
-            <Switch.Label className="text-sm leading-6 text-gray-600">
-              By selecting this, you agree to our{" "}
-              <a href="#" className="font-semibold text-indigo-600">
-                privacy&nbsp;policy
-              </a>
-              .
-            </Switch.Label>
-          </Switch.Group>
-        </div>
-        <div className="mt-10">
-          <button
-            type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Manda pra nóis
-          </button>
+          <div className="sm:col-span-2">
+            <button
+              type="submit"
+              className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              disabled={loading}
+            >
+              {loading ? <CircularProgress size={24} color="inherit" /> : "Manda pra nóis"}
+            </button>
+          </div>
         </div>
       </form>
     </div>
